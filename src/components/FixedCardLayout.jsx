b21b4f99e7a18216ab243be30ab39b12c5933e48
Leo <lddorse@gmail.com>
2025-03-26 14:05:07 -0500

    mobile menu

--- src/components/FixedCardLayout.jsx
+++ src/components/FixedCardLayout.jsx
@@ -1,19 +1,22 @@
 const FixedCardLayout = ({ children }) => {
-    return (
-      <div className="relative h-screen bg-gray-200">
-        <div
-          className="
-            fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-            w-full max-w-[768px] h-[90vh] sm:h-[595px]
-            bg-white shadow-xl rounded-xl z-20 border border-gray-300 overflow-y-auto
-            px-4 sm:px-6 py-6 text-left
-          "
-        >
-          {children}
-        </div>
+  return (
+    <div className="relative h-screen bg-gray-200">
+      {/* Top Fade Overlay */}
+      <div className="absolute top-0 left-0 w-full h-12 bg-gradient-to-b from-gray-200 to-transparent z-30 pointer-events-none" />
+
+      {/* Fixed Card Container */}
+      <div
+        className="
+          fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+          w-full max-w-[768px] h-[90vh] sm:h-[595px]
+          bg-white shadow-xl rounded-xl z-20 border border-gray-300 overflow-y-auto
+          px-4 sm:px-6 py-6 text-left
+        "
+      >
+        {children}
       </div>
-    );
-  };
-  
-  export default FixedCardLayout;
-  
+    </div>
+  );
+};
+
+export default FixedCardLayout;
