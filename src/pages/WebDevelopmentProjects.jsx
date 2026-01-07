c8c80e392ff66e8d87db324dc0a8709c18ab231a
lddorse <42456504+lddorse@users.noreply.github.com>
2026-01-07 15:48:21 -0600

    Update WebDevelopmentProjects.jsx

--- src/pages/WebDevelopmentProjects.jsx
+++ src/pages/WebDevelopmentProjects.jsx
@@ -22,7 +22,7 @@ const WebDevelopmentProjects = () => {
             target="_blank"
             rel="noopener noreferrer"
             className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-          >
+          
             Live Site
           </a>
           <Link
