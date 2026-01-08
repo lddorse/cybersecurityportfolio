fcb20c6d37a0a79898a644aa7c8aa27167ae1de3
lddorse <42456504+lddorse@users.noreply.github.com>
2026-01-08 14:27:47 -0600

    Update SecurityPolicyProject.jsx

--- src/pages/SecurityPolicyProject.jsx
+++ src/pages/SecurityPolicyProject.jsx
@@ -53,6 +53,7 @@ const SecurityPolicyProject = () => {
         <h2 className="text-xl font-semibold mb-2 text-gray-900 dark:text-gray-100">Resources</h2>
         <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-gray-300">
           <li>
+            <a
             
               href="/images/LeoDorsey_SecurityPolicyDevelopment.pdf"
               target="_blank"
